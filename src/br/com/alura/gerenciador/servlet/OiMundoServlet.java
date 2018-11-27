06237248b117531f483732ea47675ea3e0521b6f
Ramon Gimenes <ramongimee@hotmail.com>
2018-11-27 00:58:30 -0200

    Iniciando Servlet

---
+++ src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,29 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/oi")
+public class OiMundoServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+
+		PrintWriter out  = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Oi Mundo, parabéns voce escreveu o primeiro servlets");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("O Servelet OiMundoServelet foi chamado");
+	}
+	
+}
